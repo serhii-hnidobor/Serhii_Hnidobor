743da094e6625e018afb9e97619455431928ae8f
serhii_hnidobor <sergo3577@gmail.com>
2022-10-22 00:01:07 +0300

    project: * fix lint error

--- tasks/src/helpers/array/is-array-num-contain-sub-array/is-array-num-contain-sub-array.ts
+++ tasks/src/helpers/array/is-array-num-contain-sub-array/is-array-num-contain-sub-array.ts
@@ -2,7 +2,8 @@ const isArrayContainNumSubArray = (arr: number[][], targetValueArr: number[]): b
   for (const el of arr) {
     if (el[0] === targetValueArr[0] && el[1] === targetValueArr[1]) {
       return true;
-    } else if (el[1] === targetValueArr[0] && el[0] === targetValueArr[1]) {
+    }
+    if (el[1] === targetValueArr[0] && el[0] === targetValueArr[1]) {
       return true;
     }
   }
--- tasks/src/task/task5/task5.ts
+++ tasks/src/task/task5/task5.ts
@@ -5,10 +5,10 @@ export const meeting = (listStr: string): string => {
     return (friendCredentialPrev: string, friendCredentialCur: string) => {
       const [firstNamePrevFriend, lastNamePrevFriend] = friendCredentialPrev.split(':');
       const [firstNameCurFriend, lastNameCurFriend] = friendCredentialCur.split(':');
-      console.log(firstNamePrevFriend, lastNamePrevFriend, '---', firstNameCurFriend, lastNameCurFriend);
       if (!isByFirstName) {
         return lastNamePrevFriend.charCodeAt(0) - lastNameCurFriend.charCodeAt(0);
-      } else if (lastNamePrevFriend[0] === lastNameCurFriend[0]) {
+      }
+      if (lastNamePrevFriend[0] === lastNameCurFriend[0]) {
         return firstNamePrevFriend.charCodeAt(0) - firstNameCurFriend.charCodeAt(0);
       }
       return 0;
