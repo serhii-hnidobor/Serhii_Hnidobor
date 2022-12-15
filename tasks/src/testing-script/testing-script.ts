26e32fdd30e28b83ce87da9e2d17182a54f9d2c0
Serhii Hnidobor <sergo3577@gmail.com>
2022-12-15 23:35:57 +0200

    project: * remove unused fragment from prvious task

--- tasks/src/testing-script/testing-script.ts
+++
@@ -1,4 +0,0 @@
-export * from './auth/auth';
-export * from './add-salary-info/add-slary-info';
-export * from './delete-salary-info/delete-salary-info';
-export * from './delete-user-info-record/delete-user-info-record';
